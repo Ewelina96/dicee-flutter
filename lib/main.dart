1c34ecc303d2dd0256f45e0ed22fb507b5141683
Ewelina <ewelina.dziadosz@onet.pl>
2020-08-04 21:19:33 +0200

    Add simple state

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,14 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int LeftDiceNumber = 5;
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -24,9 +31,11 @@ class DicePage extends StatelessWidget {
           Expanded(
             child: FlatButton(
               onPressed: () {
-                print('left button got pressed');
+                setState(() {
+                  LeftDiceNumber = 1;
+                });
               },
-              child: Image.asset('images/dice1.png'),
+              child: Image.asset('images/dice$LeftDiceNumber.png'),
             ),
           ),
           Expanded(
